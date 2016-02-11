a58ac1ee34c13b7c03d50b1598a6142761eeaec0
jamesleong <jyleong@sfu.ca>
2016-02-11 14:35:04 +0000

    delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def my_portfolio
+    ## initiate instance variable
     
+    @user_stocks = current_user.stocks
+    @user = current_user
   end
   
 end
