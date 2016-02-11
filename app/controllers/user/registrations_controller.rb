4ba0223e45ca47a301d2d239df456882be8a5ae1
jamesleong <jyleong@sfu.ca>
2016-02-11 15:18:46 +0000

    modified signup and edit for devise controllers

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,11 @@
+class User::RegistrationsController < Devise::RegistrationsController ## inherites from devise registrations
+  before_filter :configure_permitted_parameters
+  
+  protected
+  ## need to configure for edit and sign up
+  def configure_permitted_parameters
+    devise_paramater_sanitizer.for(:sign_up).push(:first_name, :last_name)
+    devise_paramater_sanitizer.for(:account_update).push(:first_name, :last_name)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "user/registrations"}
+  ## must update our devise routes adter first and last nam added
   resources :user_stocks, except: [:show, :edit,:update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
