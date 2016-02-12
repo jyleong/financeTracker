30d09eb480c820928e67e85904a96e65e1ff9c4e
jamesleong <jyleong@sfu.ca>
2016-02-12 03:57:58 +0000

    added the UI for search friends, and its routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search" #stocks controller search action
   
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
